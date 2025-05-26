6b877429fac8b59fb23a66c58df0145c2cb729a2
PrzemekBladowski <przemek.bladowski29@gmail.com>
2025-05-26 11:34:07 +0200

    inicjalizacja projektu

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <button class="inc" @click="increments">
+    Kliknięto {{ count }} 
+    </button>
+
+    <button class="inc" @click="ShowHideUsers">
+</button>
+    <ul v-if="showUsers">
+      <li>A</li>
+      </ul>
+   
+  </div>
+</template>
+
+<script lang="ts" setup>
+const = ref(0)
+const showUsers = ref(false)
+const increments = () => {
+  count.value++
+}
+
+const ShowHideUsers = () => {
+  showUsers.value = !showUsers.value
+}

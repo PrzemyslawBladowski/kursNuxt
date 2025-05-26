f1b41b5aff476a8d3bcfada659943d3bd4c85981
PrzemekBladowski <przemek.bladowski29@gmail.com>
2025-05-26 11:39:24 +0200

    doprowadzenie projektu do czystej wersji

--- app.vue
+++ app.vue
@@ -1,26 +1,5 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <button class="inc" @click="increments">
-    Kliknięto {{ count }} 
-    </button>
-
-    <button class="inc" @click="ShowHideUsers">
-</button>
-    <ul v-if="showUsers">
-      <li>A</li>
-      </ul>
-   
   </div>
 </template>
-
-<script lang="ts" setup>
-const = ref(0)
-const showUsers = ref(false)
-const increments = () => {
-  count.value++
-}
-
-const ShowHideUsers = () => {
-  showUsers.value = !showUsers.value
-}
